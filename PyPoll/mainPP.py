3aa3d896d87f966fd68dfa4a8fb6a0bea54ebdf3
MelissaJ4 <mel.judy4@gmail.com>
2024-04-18 18:26:23 -0500

    Text file for HW

--- PyPoll/mainPP.py
+++ PyPoll/mainPP.py
@@ -3,14 +3,9 @@ import csv
 
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
-
-# Needed variables similar 
+# Needed variables for csv
 total_votes = 0
-candidates = []
-Stockham = 0
-DeGette = 0
-Doane = 0
-max_percent = 0
+max_votes = 0
 
 # Open and read csv
 with open(election_data_csv, encoding='UTF-8') as csv_file:
@@ -19,37 +14,48 @@ with open(election_data_csv, encoding='UTF-8') as csv_file:
 # Read but skip the header row first 
     csv_header = next(csv_file)
 
+# Create an open dictionary for candidates-unknown names
+    candidate_votes = {}    
 # Total number of votes cast
-# A complete list of candidates who received votes
+# A complete list of candidates who received votes-tracking candidate names
 # The total number of votes each candidate won
 
     for row in csv_reader:
-
+        # To track voting total and total votes per candidate
         total_votes +=1
-        candidates = row[2]
-    
-        if row[2] == "Stockham":
-            Stockham +=1
-        elif row[2] == "DeGette":
-            DeGette +=1
-        else row[2] == "Doane":
-            Doane +=1
-
+        name = (row[2])
+        # if loop to look for names and then add a vote once name is found
+        # keys function explained by instructor before class on 4/16
+        if name in candidate_votes.keys():
+            candidate_votes[name] += 1
+        else:
+            candidate_votes[name]=1
 
 # The percentage of votes each candidate won
-Stockham_percent = Stockham/total_votes*100
-DeGette_percent = DeGette/total_votes*100
-Doane_percent = Doane/total_votes*100
-
+    for name in candidate_votes:
+        votes_percentage = (candidate_votes[name])/total_votes
 
 # The winner of the election based on popular vote
     #candidate with max_percentage?
-
+# winner = max(candidate_votes[name])
 
 # Print the result
 
 election_summary = "Election Results"
-election_summary = 
-"----------------------------------------------------------\n"
-election_summary+=f'Total votes= {total_votes}\n'
- 
+election_summary = "----------------------------------------------------------\n"
+election_summary +=f"Total votes= {total_votes}\n"
+election_summary = "----------------------------------------------------------\n"
+for name in candidate_votes.keys():
+    election_summary+=f'{name} {candidate_votes[name]} {votes_percentage}\n'
+election_summary = "----------------------------------------------------------\n"
+# election_summary =f'"Winner" + {winner}\n'
+print("Election Results")
+print (election_summary)
+
+# Export to text file 
+#output = os.path.join('Analysis', 'Election_results')
+#with open(output, "w", newline='') as datafile:
+    #writer = csv.writer(datafile) 
+
+    #print("Election Results", file=datafile)
+    #print(bank_summary, file=datafile)
